19e59d5e9e8a93583bcc3ad8327cc6437fcb5b7f
viduc <tristan.fleury@univ-grenoble-alpes.fr>
2021-10-06 20:49:34 +0200

    init

---
+++ assets/js/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Home Component content  by : 
+    {{author_Name}} :)
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    data() {
+        var data = {
+            author_Name: "Dezz",
+        };
+        return data;
+    }
+}
+</script>
+</style>
+<style scoped>
